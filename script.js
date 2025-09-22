473604c71067d9fe1c02794e9e4ea4c65fe41aae
Aniket Yadav <117531261+aniket0657@users.noreply.github.com>
2025-09-23 00:14:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// FizzBuzz program from 1 to 20
+let result = "";
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    result += "FizzBuzz\n";
+  } else if (i % 3 === 0) {
+    result += "Fizz\n";
+  } else if (i % 5 === 0) {
+    result += "Buzz\n";
+  } else {
+    result += i + "\n";
+  }
+}
+
+// Show result in alert
+alert(result);
